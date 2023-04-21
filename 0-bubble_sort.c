6cb0436bf88fc6cc5cb644059eeb98ffdb26c4d2
Nweke Joseph <josephchristo615@gmail.com>
2023-04-21 12:46:42 +0100

    Adds task 0 file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in
+ * ascending order.
+ * @array: array of integers
+ * @size: size of the array
+ *
+ * Return: nothing.
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp;
+
+	if (!array || size < 2)
+		return;
+
+	print_array(array, size);
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+			}
+		}
+		print_array(array, size);
+	}
+
+	print_array(array, size);
+}
